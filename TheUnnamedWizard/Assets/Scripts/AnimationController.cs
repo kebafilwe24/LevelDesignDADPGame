5f51e6c57681bb6c44448f555d24a247d515a970
OmphileMatsobe <omphilejmatsobe@gmail.com>
2023-09-16 22:38:05 +0200

    character movement and animation updated but not complete.

---
+++ TheUnnamedWizard/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    [SerializeField] Animator animator;
+    Vector3 move;
+
+    bool setRun, setJump;
+
+    enum playerMove
+    {
+        foward = 1,
+        back = 2,
+        idle = 0,
+        left = 3,
+        right = 4,
+        fowardRun = 5,
+        jump = 6,
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        setRun = setJump = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+
+        move.Normalize();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            setRun = true;
+        else
+            setRun = false;
+
+        if (Input.GetKey(KeyCode.Space))
+            setJump = true;
+        else
+            setJump = false;
+
+        if (move.y != 0)
+        {
+            if (move.y > 0)
+                if (setRun == true)
+                    animator.SetInteger("playerMove", (int)playerMove.fowardRun);
+                else
+                    animator.SetInteger("playerMove", (int)playerMove.foward);
+
+            else
+                animator.SetInteger("playerMove", (int)playerMove.back);
+        }
+
+        else if (setJump == true)
+            animator.SetInteger("playerMove", (int)playerMove.jump);
+
+        else if (move == Vector3.zero)
+            animator.SetInteger("playerMove", (int)playerMove.idle);
+    }
+}
--- TheUnnamedWizard/Assets/Scripts/PlayerMovement.cs
+++ TheUnnamedWizard/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float characterSpeed;
+    [SerializeField] float runSpeed;
+    Vector3 playerMove;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void Jump()
+    {
+
+    }
+
+    void Run()
+    {
+
+    }
+
+    void Attack()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        playerMove.x = Input.GetAxis("Horizontal");
+        playerMove.z = Input.GetAxis("Vertical");
+
+        playerMove.Normalize();
+
+        this.gameObject.GetComponent<Rigidbody>().velocity = playerMove * characterSpeed ;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
 }

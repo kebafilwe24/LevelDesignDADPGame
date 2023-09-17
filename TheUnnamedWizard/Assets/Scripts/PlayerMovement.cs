9dca48f284001e593648d0bb187ed2da58a6a0aa
OmphileMatsobe <omphilejmatsobe@gmail.com>
2023-09-17 10:45:47 +0200

    Camera Movement Updated, Cinemachines to be Updated

--- TheUnnamedWizard/Assets/Scripts/PlayerMovement.cs
+++ TheUnnamedWizard/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float characterSpeed;
-    [SerializeField] float runSpeed;
-    Vector3 playerMove;
+    [SerializeField] float characterSpeed, runSpeed, rotationSpeed;
+    [SerializeField] protected Transform cameraTransform;
+    Vector3 playerMove, moveDirection;
+    float setSpeed;
+    bool enableRun = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,19 +31,46 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
+    }
+
     private void FixedUpdate()
     {
+
+        if (enableRun)
+            setSpeed = runSpeed;
+        else
+            setSpeed = characterSpeed;
+
         playerMove.x = Input.GetAxis("Horizontal");
         playerMove.z = Input.GetAxis("Vertical");
 
+
         playerMove.Normalize();
 
-        this.gameObject.GetComponent<Rigidbody>().velocity = playerMove * characterSpeed ;
+        playerMove = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * playerMove;
+
+        this.gameObject.GetComponent<Rigidbody>().velocity = playerMove * setSpeed ;
+
+        if (playerMove != Vector3.zero)
+        {
+            Quaternion characterRotation = Quaternion.LookRotation(playerMove, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation ,characterRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        if (Input.GetKey(KeyCode.LeftShift))
+            enableRun = true;
+        else
+            enableRun = false;
+        
     }
 }
